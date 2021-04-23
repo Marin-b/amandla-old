beb064957a4755c29c3fb2cc6f5259c76eb635cd
Marin Bibot <marin.bibot@hotmail.be>
2021-04-23 14:02:26 +0200

    Show

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,10 +1,14 @@
 class PlacesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index ]
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
 
   def index
     @places = Place.includes([banner_attachment: :blob]).includes(:category).all
 
     @places = @places.near(params[:location]) if params[:location].present?
     @places = @places.joins(:category).where('categories.name = ?', params[:category]) if params[:category] != 'All categories'
+  end
+
+  def show
+    @place = Place.find(params[:id])
   end
 end
